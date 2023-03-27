f125feebaddb37b50f3ba18f1392326c44e518c5
Waleed Anjum <waleedanjum@gmail.com>
2023-03-27 20:49:15 +0500

    spaces removed

--- wp-content/themes/my_custom_theme/all_featuredproduct.php
+++ wp-content/themes/my_custom_theme/all_featuredproduct.php
@@ -1,7 +1,6 @@
 <?php get_header(); ?>
 <?php /* Template Name: All Featured */ ?>
 <?php
-
 $args = array(
 	'posts_per_page' => -1,
 	'post_type'      => 'product',
@@ -16,18 +15,13 @@ $args = array(
 		)  
 );
 $featured_product = new WP_Query( $args );
-
 if ( $featured_product->have_posts() ) : 
-
 echo '<div class="woocommerce columns-4"><ul class="products">';
-
 while ( $featured_product->have_posts() ) : $featured_product->the_post();
-
 $post_thumbnail_id     = get_post_thumbnail_id();
 $product_thumbnail     = wp_get_attachment_image_src($post_thumbnail_id, $size = 'shop-feature');
 $product_thumbnail_alt = get_post_meta( $post_thumbnail_id, '_wp_attachment_image_alt', true );
 ?>
-
 	<li class="product">
 		<a href="<?php the_permalink();?>">
 			<img src="<?php echo $product_thumbnail[0];?>" alt="<?php echo $product_thumbnail_alt;?>">
@@ -37,14 +31,9 @@ $product_thumbnail_alt = get_post_meta( $post_thumbnail_id, '_wp_attachment_imag
 	</li>
 <?php 
 endwhile; 
-
 echo '</ul></div>';
-
-endif; 
-		
+endif; 	
  wp_reset_query();
  ?>          
 <!-- Featured products loop -->  
-
-
 <?php get_footer();?>
